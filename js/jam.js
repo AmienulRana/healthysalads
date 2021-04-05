aeeaf0124eec91f5b6d162f4ae32cb8d84cdab92
amienulrana <78994545+AmienulRana@users.noreply.github.com>
2021-04-05 19:14:58 +0700

    Update jam.js
    megubah format jam, yang tadinya hanya sampai 12 sekarang sudah sampai 24

--- js/jam.js
+++ js/jam.js
@@ -3,9 +3,7 @@ function realTime() {
     const clock = new Date()
     let jam = clock.getHours()
     let menit = clock.getMinutes()
-    // conver tthe hours component to 12-hour format
-    jam = (jam > 12) ? jam - 12 : jam
-    // pad the hours, minutes with leading zeros
+    jam = (jam > 24) ? jam - 24 : jam
     jam = ("0" + jam).slice(-2)
     menit = ("0" + menit).slice(-2)
     const pJam = document.querySelector('.jam')
