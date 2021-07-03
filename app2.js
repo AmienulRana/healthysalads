7eaa24f06c8f19a0227aa1d0c1f86a1da03fade0
AmienulRana <amienulrana@gmail.com>
2021-07-03 13:38:49 +0700

    =Mengatasi cors

--- app2.js
+++ app2.js
@@ -1,45 +1,72 @@
-const kategori = document.querySelectorAll('.kategori div')
-const rowMenu = document.querySelector('.row-menu')
+const kategori = document.querySelectorAll(".kategori div");
+const rowMenu = document.querySelector(".row-menu");
+
+const Allmenu = [
+  {
+    id: 1,
+    nama: "Mixed Salad",
+    harga: "20.00",
+    kategori: "salads",
+    gambar: "img/salad3.png",
+    deskripsi: " Lorem ipsum dolor sit amet consectetur adipisicing. ",
+  },
+  {
+    id: 2,
+    nama: "Kacang-kacangan",
+    harga: "24.00",
+    kategori: "salads",
+    gambar: "img/salad4.png",
+    deskripsi:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis sunt aspernatur adipisci?",
+  },
+  {
+    id: 3,
+    nama: "Avocado & Vegetables",
+    harga: "19.00",
+    kategori: "healthy food",
+    gambar: "img/salad2-removebg.png",
+    deskripsi:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis.",
+  },
+  {
+    id: 4,
+    nama: "Bayam Ungu",
+    harga: "21.00",
+    kategori: "healthy food",
+    gambar: "img/salad1.png",
+    deskripsi:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis sunt aspernatur adipisci?",
+  },
+];
+
 function allMenu() {
-    const xhttp = new XMLHttpRequest()
-    xhttp.onreadystatechange = function () {
-        if (this.status == 200 && this.readyState == 4) {
-            const result = JSON.parse(this.responseText)
-            const semuaMenu = result.menu
-            let barisMenu = ''
-            semuaMenu.forEach(menu => {
-                barisMenu += daftarMakanan(menu)  
-            })
-            rowMenu.innerHTML = barisMenu
-            const hr = document.querySelectorAll('.hr-menu')
-            hr[hr.length - 1].style.display = 'none'
-        }
-    }
-    xhttp.open('GET', 'menu.json', true)
-    xhttp.send()
+  let barisMenu = "";
+  Allmenu.forEach((menu) => {
+    barisMenu += daftarMakanan(menu);
+  });
+  rowMenu.innerHTML = barisMenu;
+  const hr = document.querySelectorAll(".hr-menu");
+  hr[hr.length - 1].style.display = "none";
 }
-allMenu()
- 
+allMenu();
 
-kategori.forEach(kat => {
-    kat.addEventListener('click', function () {
-        // menambahkan banyang pada baris menu ketika di klik
-        rowMenu.classList.add('aktif')
-        setTimeout(() => {
-            rowMenu.classList.remove('aktif')
-        }, 215)
-        
-        kategori.forEach(k => {
-            k.classList.remove('aktif')
-        });
-        this.classList.add('aktif')
-    })
-})
+kategori.forEach((kat) => {
+  kat.addEventListener("click", function () {
+    // menambahkan banyang pada baris menu ketika di klik
+    rowMenu.classList.add("aktif");
+    setTimeout(() => {
+      rowMenu.classList.remove("aktif");
+    }, 215);
 
+    kategori.forEach((k) => {
+      k.classList.remove("aktif");
+    });
+    this.classList.add("aktif");
+  });
+});
 
 function daftarMakanan(menu) {
-    return (
-        `<div class="row mt-1 justify-content-between">
+  return `<div class="row mt-1 justify-content-between">
             <div class="col-6">
                 <img src="${menu.gambar}" data-imgId=${menu.id} class="img-fluid img-app mt-2">
             </div>
@@ -49,52 +76,23 @@ function daftarMakanan(menu) {
                 <button class="button-tambah tambah-menu" data-idbtn=${menu.id}><h6 style="font-size:12px;">tambah</h6></button>
             </div>
         </div>
-        <hr class="hr-menu">`
-    )
+        <hr class="hr-menu">`;
 }
 
-kategori.forEach(kat => {
-    kat.addEventListener('click', function () {
-        const namaKategori = kat.dataset.kategori
-        if(namaKategori == 'all'){
-            allMenu()
-            return
-        }
-        const xhr = new XMLHttpRequest()
-        xhr.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                const result = JSON.parse(this.responseText)
-                const semuaMenu = result.menu
-                let content = ''
-                semuaMenu.forEach(menu => {
-                    const kategori = menu.kategori
-                    if (namaKategori == kategori) {
-                        content += daftarMakanan(menu)
-                    }
-                    rowMenu.innerHTML = content
-                })
-            }
-        }
-        xhr.open('GET', 'menu.json', true)
-        xhr.send()
-    })
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+kategori.forEach((kat) => {
+  kat.addEventListener("click", function () {
+    const namaKategori = kat.dataset.kategori;
+    if (namaKategori == "all") {
+      allMenu();
+      return;
+    }
+    let content = "";
+    Allmenu.forEach((menu) => {
+      const kategori = menu.kategori;
+      if (namaKategori == kategori) {
+        content += daftarMakanan(menu);
+      }
+      rowMenu.innerHTML = content;
+    });
+  });
+});
